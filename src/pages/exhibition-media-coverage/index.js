3e69e52485d7e8b5e7ce55bdb8806e22fff0ee7a
Apocryphon@06 <hrithikchandrashekar@gmail.com>
2025-12-17 23:14:05 +0530

    new pages

---
+++ src/pages/exhibition-media-coverage/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExhibitionMediaCoverage = () => {
+  return (
+    <div>ExhibitionMediaCoverage</div>
+  )
+}
+
+export default ExhibitionMediaCoverage
---
+++ src/pages/experience-center/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExperienceCenter = () => {
+  return (
+    <div>ExperienceCenter</div>
+  )
+}
+
+export default ExperienceCenter
