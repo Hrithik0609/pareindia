de1f209cba37aaa7888109a78a577e8958d7fa91
Apocryphon@06 <hrithikchandrashekar@gmail.com>
2026-01-10 21:29:30 +0530

    cleanup

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,77 +1,9 @@
-import React from 'react';
-import { useTheme } from '@/store/useTheme';
-import { useParams } from '@/store/useParams';
-import PrimaryButton from '@/shared/PrimaryButton';
+import React from 'react'
 
-export async function getServerSideProps() {
-
-    try {
-        let data = await fetch('https://jsonplaceholder.typicode.com/posts')
-        data = await data.json()
-        if (!data) {
-            throw new Error('Something went wrong')
-        }
-
-        return {
-            props: {
-                posts: data
-            }
-        }
-    }
-    catch (e) {
-        console.log(e)
-    }
-
-    return {
-        props: {
-            posts: []
-        }
-    }
-}
-
-const Test = ({ posts }) => {
-
-    const { theme, toggleLight, toggleDark } = useTheme()
-
-    const { params, handleChange } = useParams()
-
-    console.log(posts, 'posts')
-
-    return (
-        <div className='flex flex-col items-center justify-center w-full h-screen gap-4 p-4'>
-
-            <h2 className='text-xl font-semibold'>{theme}</h2>
-            <PrimaryButton onClick={toggleLight} width="lg:w-60 w-full" label="Light" />
-            <PrimaryButton onClick={toggleDark} width="lg:w-60 w-full" label="Dark" />
-
-            <input
-                name="name"
-                value={params?.name}
-                onChange={handleChange}
-                placeholder="Name"
-                className='p-3 outline-none border border-[#808080] w-full lg:w-60'
-            />
-
-            <input
-                name="age"
-                value={params?.age}
-                onChange={handleChange}
-                placeholder="Age"
-                className='p-3 outline-none border border-[#808080] w-full lg:w-60'
-            />
-
-
-            <input
-                name="phone"
-                value={params?.phone}
-                onChange={handleChange}
-                placeholder="Phone Number"
-                className='p-3 outline-none border border-[#808080] w-full lg:w-60'
-            />
-
-
-        </div>
-    )
+const Test = () => {
+  return (
+    <div>Test</div>
+  )
 }
 
 export default Test
